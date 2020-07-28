29772fd9b63fccfc556241fc58ea16e5a6ce8e52
cperla4 <68865724+cperla4@users.noreply.github.com>
2020-07-28 13:05:47 -0400

    Update keychain.py

--- keychain.py
+++ keychain.py
@@ -1,7 +1,12 @@
 from netmiko import ConnectHandler
 import requests
-import os, sys, time
+from datetime import datetime
+import os, sys
 
-def current_time():
-  pass
-  
+def current_time(i):
+    month_list = ['Void','Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    current_month = month_list[i.month]
+    current_day = i.day
+    current_year = i.year 
+    return '%s %s %s' % (current_month, current_day, current_year)
+date_now = current_time(datetime.now())
