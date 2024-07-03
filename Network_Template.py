5a6de45e79f6e5fd9754879c7e6fb56e4c23629f
cperla4 <68865724+cperla4@users.noreply.github.com>
2024-07-03 12:18:19 -0400

    Update Network_Template.py

--- Network_Template.py
+++ Network_Template.py
@@ -1,1 +1,10 @@
+__author__ = "cyro"
+if __name__ == "__main__":
 
+    ios_connect = {
+        'device_type' : 'cisco_ios',
+        'host' : 'ip',
+        'username' : 'username',
+        'password' : 'password',
+        'port' : '22',
+    }
