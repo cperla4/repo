1ea5b83c3bf5eb8d4f949bc6b6e630f2b582eb63
cperla4 <68865724+cperla4@users.noreply.github.com>
2024-07-03 12:58:36 -0400

    Update Network_Template.py

--- Network_Template.py
+++ Network_Template.py
@@ -1,8 +1,10 @@
 __author__ = "Christian.Perla"
 if __name__ == "__main__":
 
-    from getpass import getpass
-    
+    import sys    #System functions
+    from getpass import getpass    #Hides clear-text passwords
+
+    #Terminal Session Parameters
     ios_connect = {
         'device_type' : 'cisco_ios',
         'host' : 'ip',
@@ -10,18 +12,23 @@ if __name__ == "__main__":
         'password' : 'password',
         'port' : '22',
     }
+    #Provided Operator Variables
     user_id = input('Username: ')
     pass_id = getpass('Password: ')
     host_id = input('IP: ')
-    
+
+    #Provided Operator Input
     ios_connect['username'] = user_id
     ios_connect['password'] = pass_id
     ios_connect['host'] = host_id
     
     ssh_connect = ConnectHandler(**ios_connect)    #Runs IOS Parameters against Netmiko Terminal
     ssh_output = ssh_connect.send_command('cli show commands')    #Runs show commands in terminal
-    ssh_connect.disconnect()
+    
+    ssh_connect.disconnect()    #Kills Terminal Session
 
     print(ssh_output)    #Print results in terminal
+
+    sys.exit(0)
     
     
