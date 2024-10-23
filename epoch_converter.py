63b1592e7768d6eb10a3d7ffe9bec07381e7c4b1
cperla4 <68865724+cperla4@users.noreply.github.com>
2024-10-22 20:25:42 -0400

    Update epoch_converter.py

--- epoch_converter.py
+++ epoch_converter.py
@@ -1,4 +1,6 @@
 import time
 
-epoch_time = input()
+epoch_time = int(input())
 datetime_object = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch_time))
+
+print(datetime_object) #Just for verification
